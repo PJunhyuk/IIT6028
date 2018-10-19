5b1827705c64878e093bc0d822f2bb890a31211f
Junhyuk Park <jgravity@naver.com>
2018-10-20 00:10:46 +0900

    Initialize A3

---
+++ A3/src/starter.m
@@ -0,0 +1,41 @@
+% starter script for project 3
+DO_TOY = true;
+DO_BLEND = false;
+DO_MIXED  = false;
+DO_COLOR2GRAY = false;
+
+if DO_TOY 
+    toyim = im2double(imread('./samples/toy_problem.png')); 
+    % im_out should be approximately the same as toyim
+    im_out = toy_reconstruct(toyim);
+    disp(['Error: ' num2str(sqrt(sum(toyim(:)-im_out(:))))])
+end
+
+if DO_BLEND
+    im_background = imresize(im2double(imread('./samples/im2.jpg')), 0.5, 'bilinear');
+    im_object = imresize(im2double(imread('./samples/penguin-chick.jpeg')), 0.5, 'bilinear');
+
+    % get source region mask from the user
+    objmask = getMask(im_object);
+    % align im_s and mask_s with im_background
+    [im_s, mask_s] = alignSource(im_object, objmask, im_background);
+
+    % blend
+    im_blend = poissonBlend(im_s, mask_s, im_background);
+    figure(3), hold off, imshow(im_blend)
+end
+
+if DO_MIXED
+    % read images
+    %...
+    
+    % blend
+    im_blend = mixedBlend(im_s, mask_s, im_bg);
+    figure(3), hold off, imshow(im_blend);
+end
+
+if DO_COLOR2GRAY
+    im_rgb = im2double(imread('./samples/colorBlindTest35.png'));
+    im_gr = color2gray(im_rgb);
+    figure(4), hold off, imagesc(im_gr), axis image, colormap gray
+end
